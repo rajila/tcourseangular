c9f524f731c2484b1986f181e6b27213369c5f4c
Ronald <rdajila@gmail.com>
2021-05-17 14:24:42 +0200

    Add module for routing: ng generate module app-routing --flat --module=app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
+// Un componentes controla la zona de espacio de la pantalla que representa la vista
+// Define las propiedades y metodos que se usan en el template
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+@Component({ // Anotaciones para el componente
+  selector: 'app-root', // etiqueta raiz
+  templateUrl: './app.component.html', // Vista
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent { // Controlador
   title = 'tcourseangular';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser'; // Para renderizar la app en el navegador
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
+  declarations: [ // define las vistas que contiene el modulo
     AppComponent
   ],
-  imports: [
-    BrowserModule
+  exports: [], // declaraciones que son visibles para componentes de otros modulos
+  imports: [ // clases que otros modulos exportan para utilizarlas en el modulo actual
+    BrowserModule, AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], // servicios usados de forma global y accesibles desde cualquier parte de la app
+  bootstrap: [AppComponent] // vista principal del app. Solo es definida por el modulo principal
 })
 export class AppModule { }
