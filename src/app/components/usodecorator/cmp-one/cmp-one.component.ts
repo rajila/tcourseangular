b810baaf35575e1794f05ef1b21617bd1955aac6
Ronald <rdajila@gmail.com>
2021-05-23 11:17:13 +0200

    Example comunication component Input/Output

--- src/app/components/usodecorator/cmp-one/cmp-one.component.ts
+++ src/app/components/usodecorator/cmp-one/cmp-one.component.ts
@@ -10,12 +10,28 @@ export class CmpOneComponent implements OnInit {
   public country: string;
   public mother: string;
 
+  public valueOne: number;
+  public valueTwo: number;
+  public resultOperando: number;
+  public lblResult: string;
+
+  public dataJson: Object;
+
   constructor() { 
     this.country = 'Ecuador';
     this.mother = 'Aura';
+    this.valueOne = 0;
+    this.valueTwo = 0;
+    this.resultOperando = 0;
+    this.lblResult = 'X ? Y = ';
+    this.dataJson = {name:'Ronald', surname:'Ajila'};
   }
 
   ngOnInit(): void {
   }
 
+  catchEvent(data: any): void {
+    this.resultOperando = data.result;
+    this.lblResult = 'X ' + data.operando + ' Y = ';
+  }
 }
--- src/app/components/usodecorator/cmp-two/cmp-two.component.ts
+++ src/app/components/usodecorator/cmp-two/cmp-two.component.ts
@@ -1,15 +1,22 @@
-import { Attribute, Component, HostListener, Input, OnInit } from '@angular/core';
+import { Attribute, Component, EventEmitter, HostListener, Input, OnChanges, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cmp-two',
   templateUrl: './cmp-two.component.html',
   styleUrls: ['./cmp-two.component.css']
 })
-export class CmpTwoComponent implements OnInit {
+export class CmpTwoComponent implements OnInit, OnChanges {
 
-  // @Attribute('customSurnameAttr') public surnameAttr: string; // Its necesary put in contructor for valur of component one
+  // @Attribute('customSurnameAttr') public surnameAttr: string; // Its necesary put in contructor for value of component one
   @Input() public countryIn: string;
   @Input() public motherIn: string; // Error put on constructor
+
+  @Input() public valueOneIn: number;
+  @Input() public valueTwoIn: number;
+
+  @Input() public dataJson: Object;
+
+  @Output() public sendData: EventEmitter<any>; 
 
   public contadorClick: number;
   public contadorMouse: number;
@@ -19,11 +26,20 @@ export class CmpTwoComponent implements OnInit {
   ) {
     this.countryIn = 'Spain'; // No change value. Take value of component One
     this.motherIn = 'Esperanza'; // No change value. Take value of component One
+    this.valueOneIn = 0;
+    this.valueTwoIn = 0;
     this.contadorClick = 0;
     this.contadorMouse = 0;
+    this.sendData = new EventEmitter();
+    this.dataJson = {}; // if dataJson no recibe params init
   }
 
   ngOnInit(): void {
+    console.error(typeof this.dataJson);
+    console.log(this.dataJson);
+  }
+
+  ngOnChanges(): void {
   }
 
   @HostListener('click', ['$event'])
@@ -38,4 +54,16 @@ export class CmpTwoComponent implements OnInit {
     this.contadorMouse++;
   }
 
+  onClickData($event: any): void {
+    let operator = $event.target.getAttribute('operator');
+    if (operator === '+') {
+      this.sendData.emit({result: this.valueOneIn+this.valueTwoIn, operando: operator});
+    } else if (operator === '-') {
+      this.sendData.emit({result: this.valueOneIn-this.valueTwoIn, operando: operator});
+    } else if (operator === '*') {
+      this.sendData.emit({result: this.valueOneIn*this.valueTwoIn, operando: operator});
+    } else {
+      this.sendData.emit({result: this.valueOneIn/this.valueTwoIn, operando: operator});
+    }
+  }
 }
