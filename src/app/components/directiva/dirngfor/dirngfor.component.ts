8aafa1663e7bb0f9525a400c6d9e776d00525cf3
Ronald <rdajila@gmail.com>
2021-06-02 15:11:24 +0200

    Test on ngFor, filter data before looping

--- src/app/components/directiva/dirngfor/dirngfor.component.ts
+++ src/app/components/directiva/dirngfor/dirngfor.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirngforComponent implements OnInit {
 
-  public lNotes: Array<Object>;
+  public lNotes!: Array<Object>;
 
   constructor() { 
     this.lNotes = new Array();
@@ -24,4 +24,17 @@ export class DirngforComponent implements OnInit {
     this.lNotes.push({name: 'Charlotte', note: 9});
     this.lNotes.push({name: 'Isabella', note: 10});
   }
+
+  // No preprocesar la información de esta manera ya que se invoca el metodo 2 veces el *ngFor
+  getDataCustom(name: string): string {
+    console.error(name);
+    return 'Hi ' + name + '. What is up?';
+  }
+
+  // Forma para preprocesar la información: desde *ngFor se llama: trackBy: preProcessNote;
+  preProcessNote(index: number, note: any): void {
+    note.total = 100; // Add property
+    console.error(note);
+  }
+
 }
