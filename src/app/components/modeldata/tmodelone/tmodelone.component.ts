d0e35ad67ed6b82bf6d9896add39dc377812bc32
Ronald <rdajila@gmail.com>
2021-06-15 06:30:24 +0200

    Services: Using Observables

--- src/app/components/modeldata/tmodelone/tmodelone.component.ts
+++ src/app/components/modeldata/tmodelone/tmodelone.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Book } from 'src/app/models/book';
 import { BookService } from 'src/app/service/book.service';
 
@@ -13,23 +14,41 @@ export class TmodeloneComponent implements OnInit {
   public lbooks: Array<Book>;
   public dimBooks: Number;
 
+  public observableSubOneToOne!: Subscription;
+  public lbooksOS: Array<Book>;
+
+  public observableSubAll!: Subscription;
+  public lbooksOSAll: Array<Book>;
+
   constructor(private _bookService: BookService) { 
-    this.lbooks = _bookService.getBooks();
+    this.lbooks = _bookService.getBooks(); // Obtiene los libros normalmente
     this.dimBooks = 0;
+    this.lbooksOS = [];
+    this.lbooksOSAll = [];
   }
 
   ngOnInit(): void {
-    // this.initBook();
+    // Using Promises
     // then -> retorna otra promesa y poder realizar un encadenamiento de promesas
     this._bookService.getBooksPromise().then(data => this.dimBooks = data.length);
+    
+    // Using Observables: Recibe los libros de uno en uno
+    this.observableSubOneToOne = this._bookService.getBookObservableOneToOne().subscribe(
+      book => this.lbooksOS.push(book), // Get data del observable
+      error => console.error(error), // Catch ERROR
+      () => console.info('Finalización Book One to One') // Catch FIN de envio de datos del observable
+    );
+
+    // Using Observables: Recibe todos los libros
+    this.observableSubAll = this._bookService.getBookObservableAll().subscribe(
+      books => this.lbooksOSAll = books,
+      error => console.error(error),
+      () => console.info('Finalización All Books')
+    );
   }
 
-  initBook(): void {
-    this.lbooks.push(new Book(1, 'El Quijote', 'Cuento'));
-    this.lbooks.push(new Book(2, 'Hamiet', 'Venganza'));
+  ngOnDestroy(): void {
+    if (this.observableSubOneToOne) this.observableSubOneToOne.unsubscribe();
+    if (this.observableSubAll) this.observableSubAll.unsubscribe();
   }
-
-  // ngOnDestroy(): void {
-  //   this.lbooks = [];
-  // }
 }
--- src/app/service/book.service.ts
+++ src/app/service/book.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Book } from '../models/book';
 import { Util } from '../util/util';
 
@@ -7,14 +8,37 @@ export class BookService {
 
   constructor() { }
 
+  // Get Books normal
   getBooks(): Array<Book> {
     return Util.Books;
   }
 
-  // Get Books using Promise
+  // Get Books using Promise: Send all books.
   getBooksPromise(): Promise<Array<Book>> {
     return new Promise<Array<Book>>((resolve, reject) => {
-      setTimeout(() => {resolve(Util.Books);}, 10000);
+      setTimeout(() => {resolve(Util.Books);}, 5000);
+    });
+  }
+
+  // Get Books using Observables: Book one to one. Case one: Envia libro, uno a uno
+  getBookObservableOneToOne(): Observable<Book> {
+    return new Observable<Book>( observer => {
+      // Retorna el libro uno a uno
+      Util.Books.forEach((book, index) => {
+        setTimeout(() => {
+          observer.next(book); // El observable retorna el libro cada (index + 1) * 1500 segundos
+        }, (index + 1) * 2000);
+      });
+      setTimeout(() => {
+        observer.complete(); // Indica que el Observable ha dejado de enviar datos
+      }, (Util.Books.length + 1) * 2000);
+    });
+  }
+
+  // Get Books using Observables: Send all books. Case two: Envia todos los libros
+  getBookObservableAll(): Observable<Array<Book>> {
+    return new Observable<Array<Book>>( observer => {
+      setTimeout(() => { observer.next(Util.Books) }, 2000);
     });
   }
 }
