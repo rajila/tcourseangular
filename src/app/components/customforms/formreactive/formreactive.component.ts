9f4b653f51518796642069a648e5e21bdcd46d59
Ronald <rdajila@gmail.com>
2021-06-24 17:06:10 +0200

    Forms: LocalStorage

--- src/app/components/customforms/formreactive/formreactive.component.ts
+++ src/app/components/customforms/formreactive/formreactive.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { data } from 'jquery';
 
 import { User } from 'src/app/interfaces/user';
 import { Validations } from 'src/app/util/validations';
@@ -43,5 +44,24 @@ export class FormreactiveComponent implements OnInit {
     this.userData.code = this.form.controls['code'].value;
     this.userData.name = this.form.controls.name.value;
     console.info(this.userData);
+    localStorage.setItem('user', JSON.stringify(this.userData));
+    this.form.reset();
+  }
+
+  loadData(): void {
+    let dataUserLS = localStorage.getItem('user');
+    console.log(dataUserLS);
+    if (dataUserLS) {
+      let dataUser = JSON.parse(dataUserLS);
+      this.form.patchValue({code: dataUser.code});
+      this.form.patchValue({name: dataUser.name});
+    }
+    localStorage.clear();
+  }
+
+  get existUser() {
+    let dataLS = localStorage.getItem('user'); // NULL or {}
+    if (dataLS) dataLS = JSON.parse(dataLS);
+    return dataLS;
   }
 }
